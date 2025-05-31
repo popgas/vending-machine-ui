721bb5d7e69e2a52c474ff0bdd185194d5cb1000
Leonardo Caldas <leonardo.ferreira.caldas@gmail.com>
2025-05-31 17:58:43 -0300

    feat: corrigindo camera

--- infrastructure/hardware/audio.py
+++ infrastructure/hardware/audio.py
@@ -24,7 +24,24 @@ class AudioWorker:
         if pygame.mixer.get_init():
             pygame.mixer.quit()
 
-        pygame.mixer.init()
+        pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=4096)
+
+        try:
+            print("Tentando inicializar mixer com SDL_AUDIODRIVER =", pygame.get_sdl_audio_driver())
+        except Exception:
+            print("SDL_AUDIODRIVER não definido. Forçando 'alsa'.")
+
+            import os
+            os.environ["SDL_AUDIODRIVER"] = "alsa"
+            print("SDL_AUDIODRIVER agora =", os.environ["SDL_AUDIODRIVER"])
+
+        try:
+            pygame.mixer.init()
+            print("Mixer inicializado com sucesso.")
+        except pygame.error as e:
+            print(f"Erro ao inicializar mixer: {e}", file=sys.stderr)
+            return
+
         pygame.mixer.music.stop()
         pygame.mixer.music.load(path)
         pygame.mixer.music.play()
